81bd1e2870e04ed1b39cb9c18a53eea21f631df0
Jhon Paul <pablodr53@hotmail.com>
2025-02-09 22:40:32 -0500

    Mejoras en la animación inicial

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 const title = document.querySelector('.title');
 const text = ['I', 'Have', 'Something', 'For', 'You'];
 
-// Usa un span con `white-space: pre;` para mantener los espacios entre palabras
-title.innerHTML = text.map(word => `<span style="white-space: pre;">${word} </span>`).join('');
+// Usamos spans y añadimos un pequeño espacio después de cada palabra
+title.innerHTML = text.map(word => `<span class="word">${word}</span>`).join(' ');
 
 const textElements = document.querySelectorAll('.title span');
-textElements.forEach((element) => {
+textElements.forEach((element, index) => {
   const randomDelay = Math.random() * 3; // Genera un delay aleatorio entre 0 y 3 segundos
   element.style.animationDelay = `${randomDelay}s`;
 });
