012a5c4fbd7adeffc74070854b0e710bdd040593
Jhon Paul <pablodr53@hotmail.com>
2025-02-12 01:23:07 -0500

    Mejorando mensaje flowers era vez

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY BIRTHDAY,HONEY! Miss Tunqui').split('')
+    const titles = ('HAPPY BIRTHDAY,HONEY! MissTunqui').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
