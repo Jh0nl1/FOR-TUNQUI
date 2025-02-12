26ed02f4b5abf55cf2e7e103ef3ec4b99819900d
Jhon Paul <pablodr53@hotmail.com>
2025-02-12 01:44:54 -0500

    Corrección de animación en el título y ajuste en saltos de línea

--- js/main.js
+++ js/main.js
@@ -3,15 +3,24 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY BIRTHDAY,HONEY! MissTunqui').split('')
-    const titleElement = document.getElementById('title');
+    // Definir el texto con saltos de línea
+    const titles = "HAPPY BIRTHDAY, HONEY!<br>Miss Tunqui";
+    const titleElement = document.getElementById("title");
     let index = 0;
 
     function appendTitle() {
       if (index < titles.length) {
-        titleElement.innerHTML += titles[index];
-        index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
+        if (titles[index] === "<") {
+          // Si detecta un tag de HTML (como <br>), agrégalo de una vez
+          let endIndex = titles.indexOf(">", index);
+          titleElement.innerHTML += titles.substring(index, endIndex + 1);
+          index = endIndex + 1;
+        } else {
+          // Añade letra por letra con animación
+          titleElement.innerHTML += titles[index];
+          index++;
+        }
+        setTimeout(appendTitle, 300); // Velocidad de la animación
       }
     }
 
