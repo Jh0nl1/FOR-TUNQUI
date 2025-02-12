0d08363108f410b475c3b537c42fb4403cd28497
Jhon Paul <pablodr53@hotmail.com>
2025-02-12 01:10:11 -0500

    Mejorando mensaje flowers 2da vezz

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY BIRTHDAY, HONEY! Always Thinking of You, Tunqui').split('')
+    const titles = ('HAPPY BIRTHDAY,HONEY! Miss Tunqui').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
