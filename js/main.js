4645e845748a9172e71b6e8ba0550ea88ae2270d
Jhon Paul <pablodr53@hotmail.com>
2025-02-12 20:48:03 -0500

    Ajuste tamaño y posición del texto en flower.html

--- js/main.js
+++ js/main.js
@@ -3,20 +3,20 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titleText = "HAPPY BIRTHDAY, HONEY! Miss Tunqui";
-    const titleElement = document.getElementById("title");
+    const titles = ('HAPPY BIRTHDAY, HONEY! Miss Tunqui').split('')
+    const titleElement = document.getElementById('title');
     let index = 0;
 
     function appendTitle() {
-      if (index < titleText.length) {
-        titleElement.innerHTML += titleText[index] === " " ? "&nbsp;" : titleText[index];
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 300); // Reduce el delay para que sea más fluido
+        setTimeout(appendTitle, 300); // 1000ms delay
       }
     }
 
     appendTitle();
+
     clearTimeout(c);
   }, 1000);
 };
-
