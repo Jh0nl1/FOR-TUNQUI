d1ae21e7c4e08a73fe500aa2a4dedad172f0cc8a
Jhon Paul <pablodr53@hotmail.com>
2025-02-12 20:28:32 -0500

    Ajuste de tamaño de texto en flower.html y mejora animación

--- js/main.js
+++ js/main.js
@@ -3,29 +3,20 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    // Definir el texto con saltos de línea
-    const titles = "HAPPY BIRTHDAY, HONEY!<br>Miss Tunqui";
+    const titleText = "HAPPY BIRTHDAY, HONEY! Miss Tunqui";
     const titleElement = document.getElementById("title");
     let index = 0;
 
     function appendTitle() {
-      if (index < titles.length) {
-        if (titles[index] === "<") {
-          // Si detecta un tag de HTML (como <br>), agrégalo de una vez
-          let endIndex = titles.indexOf(">", index);
-          titleElement.innerHTML += titles.substring(index, endIndex + 1);
-          index = endIndex + 1;
-        } else {
-          // Añade letra por letra con animación
-          titleElement.innerHTML += titles[index];
-          index++;
-        }
-        setTimeout(appendTitle, 300); // Velocidad de la animación
+      if (index < titleText.length) {
+        titleElement.innerHTML += titleText[index] === " " ? "&nbsp;" : titleText[index];
+        index++;
+        setTimeout(appendTitle, 300); // Reduce el delay para que sea más fluido
       }
     }
 
     appendTitle();
-
     clearTimeout(c);
   }, 1000);
 };
+
